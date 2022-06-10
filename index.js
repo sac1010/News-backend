0687bca229608c6196be2bddab32c75ba5ffbe47
sac1010 <83954950+sac1010@users.noreply.github.com>
2022-06-11 01:19:29 +0530

    heroku fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 
 app.use("/", newsController)
 app.use(cors())
-const port = process.env.port || 3001
+const port = process.env.PORT || 3001
 
 app.listen(port, ()=>{
     console.log("listening on 3001")
