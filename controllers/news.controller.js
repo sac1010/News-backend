5d069418f1f992983afab1f2b0065149c782ff42
sac1010 <83954950+sac1010@users.noreply.github.com>
2022-06-11 13:27:40 +0530

    added route for metadata

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const axios = require("axios")
 const router = express.Router()
+const urlMetadata = require('url-metadata')
 
 router.get("/topstories", async(req, res)=>{
     try{
@@ -77,6 +78,16 @@ router.get("/beststories", async(req, res)=>{
 
 
 })
+
+router.post("/metadata", async(req, res)=>{
+    try{
+        const url = req.body.url
+         const metaData = await urlMetadata(url) 
+         res.send(metaData)    
+    }catch(e){
+        res.status(500).send(e)
+    }
+}) 
  
 
 module.exports = router
