d1d0ac558e6a6ad6094d40b2b9af748455f15e68
sac1010 <83954950+sac1010@users.noreply.github.com>
2022-06-12 11:13:01 +0530

    removed redundant codes

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,93 +1,50 @@
-const express = require("express")
-const axios = require("axios")
-const router = express.Router()
-const urlMetadata = require('url-metadata')
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
+const urlMetadata = require("url-metadata");
 
-router.get("/topstories", async(req, res)=>{
-    try{
-        const response = await axios.get(`https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`)
-        const storyIds = response.data
-        const size = 8
-        const pages = Math.ceil(storyIds.length/size) 
-        const page = req.query.page || 1
-        const pageIds = storyIds.splice((page-1)*size, size)
-    
-                const promises = []
-                for(let i=0; i<pageIds.length; i++){
-                    const result =  axios.get(`https://hacker-news.firebaseio.com/v0/item/${pageIds[i]}.json?print=pretty`)
-                    promises.push(result)
-                    
-                }
-                const results = await Promise.all(promises);
-                const stories  = results.map((res)=>res.data) 
-        res.status(200).send({stories, pages})
-    }catch(err){
-        res.send(err)
+router.get("/:newstype", async (req, res) => {
+  try {
+    newstypes = {
+      topstories: "topstories",
+      newstories: "newstories",
+      beststories: "beststories",
+    };
+    const newstype = newstypes[req.params.newstype];
+    const response = await axios.get(
+      `https://hacker-news.firebaseio.com/v0/${newstype}.json?print=pretty`
+    );
+    const storyIds = response.data;
+    const size = 8;
+    const pages = Math.ceil(storyIds.length / size);
+    const page = req.query.page || 1;
+    const pageIds = storyIds.splice((page - 1) * size, size);
+
+    const promises = [];
+    for (let i = 0; i < pageIds.length; i++) {
+      const result = axios.get(
+        `https://hacker-news.firebaseio.com/v0/item/${pageIds[i]}.json?print=pretty`
+      );
+      promises.push(result);
     }
+    const results = await Promise.all(promises);
+    const stories = results.map((res) => res.data);
+    res.status(200).send({ stories, pages });
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 
-})
 
-router.get("/newstories", async(req, res)=>{
-    
-    try{
-        const response = await axios.get(`https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty`)
-        const storyIds = response.data
-        const size = 8
-        const pages = Math.ceil(storyIds.length/size) 
-        const page = req.query.page || 1
-        const pageIds = storyIds.splice((page-1)*size, size)
-    
-                const promises = []
-                for(let i=0; i<pageIds.length; i++){
-                    const result =  axios.get(`https://hacker-news.firebaseio.com/v0/item/${pageIds[i]}.json?print=pretty`)
-                    promises.push(result)
-                    
-                }
-                const results = await Promise.all(promises);
-                const stories  = results.map((res)=>res.data) 
-        res.status(200).send({stories, pages})
-    }catch(err){
-        res.status(500).send(err)
-    }
+router.post("/metadata", async (req, res) => {
+  try {
+    const url = req.body.url;
+    const metaData = await urlMetadata(url);
+    res.send(metaData);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
-
-})
-
-router.get("/beststories", async(req, res)=>{
-    try{
-        const response = await axios.get(`https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty`)
-        const storyIds = response.data
-        const size = 8
-        const pages = Math.ceil(storyIds.length/size) 
-        const page = req.query.page || 1
-        const pageIds = storyIds.splice((page-1)*size, size)
-    
-                const promises = []
-                for(let i=0; i<pageIds.length; i++){
-                    const result =  axios.get(`https://hacker-news.firebaseio.com/v0/item/${pageIds[i]}.json?print=pretty`)
-                    promises.push(result)
-                    
-                }
-                const results = await Promise.all(promises);
-                const stories  = results.map((res)=>res.data) 
-        res.status(200).send({stories, pages})
-    }catch(err){
-        res.send(err)
-    }
-
-
-})
-
-router.post("/metadata", async(req, res)=>{
-    try{
-        const url = req.body.url
-         const metaData = await urlMetadata(url) 
-         res.send(metaData)    
-    }catch(e){
-        res.status(500).send(e)
-    }
-}) 
- 
-
-module.exports = router
+module.exports = router;
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require("express")
-const app = express()
-const newsController = require("./controllers/news.controller")
-const cors = require("cors")
-app.use(express.json())
-app.use(cors())
+const express = require("express");
+const app = express();
+const newsController = require("./controllers/news.controller");
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
 
-app.use("/", newsController)
-const port = process.env.PORT || 3001
+app.use("/", newsController);
+const port = process.env.PORT || 3001;
 
-app.listen(port, ()=>{
-    console.log("listening on 3001")
-})
-
+app.listen(port, () => {
+  console.log("listening on 3001");
+});
